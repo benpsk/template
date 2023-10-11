3efbbd6fbe66cc31fa1a18013bfb6e36fa16d6c9
Ben Gunn <bengunn.dev@gmail.com>
2023-10-11 21:57:00 +0630

    add: sidebar toggle

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 function menuOnClick() {
-  console.log('menuOnClick');
-  document.getElementById("menu-bar").classList.toggle("change");
+  console.log("menuOnClick");
+  document.querySelector(".menu-bar").classList.toggle("menu-bar-close");
+  document.querySelector(".sidebar").classList.toggle("sidebar-collape");
+  document
+    .querySelector(".main-content")
+    .classList.toggle("main-content-collape");
+  document.querySelectorAll(".menu-text").forEach((item) => {
+    item.classList.toggle("menu-text-hide");
+  });
+  document.querySelector(".header-title").classList.toggle("header-title-hide");
 }
